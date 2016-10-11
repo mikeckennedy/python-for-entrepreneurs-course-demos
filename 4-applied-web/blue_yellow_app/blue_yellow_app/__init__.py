2016d21ee80fa10e3c728e7d950113a7b7ad1e95
Michael Kennedy <mikeckennedy@gmail.com>
2016-10-11 11:17:39 -0700

    recorded demo using pyramid handlers and base controllers

--- 4-applied-web/blue_yellow_app/blue_yellow_app/__init__.py
+++ 4-applied-web/blue_yellow_app/blue_yellow_app/__init__.py
@@ -1,14 +1,29 @@
 from pyramid.config import Configurator
+import blue_yellow_app.controllers.home_controller as home
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+
+    init_includes(config)
+    init_routing(config)
+
+    return config.make_wsgi_app()
+
+
+def init_routing(config):
+    config.add_static_view('static', 'static', cache_max_age=3600)
+
+    config.add_handler('root', '/', handler=home.HomeController, action='index')
+    config.add_handler('home_ctrl', '/home/{action}', handler=home.HomeController)
+    config.add_handler('home_ctrl/', '/home/{action}/', handler=home.HomeController)
+    config.add_handler('home_ctrl_id', '/home/{action}/{id}', handler=home.HomeController)
+
+    config.scan()
+
+
+def init_includes(config):
     config.include('pyramid_chameleon')
     config.include('pyramid_handlers')
-
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.scan()
-    return config.make_wsgi_app()
---
+++ 4-applied-web/blue_yellow_app/blue_yellow_app/controllers/base_controller.py
@@ -0,0 +1,12 @@
+import blue_yellow_app.infrastructure.static_cache as static_cache
+from blue_yellow_app.infrastructure.supressor import suppress
+
+
+class BaseController:
+    def __init__(self, request):
+        self.request = request
+        self.build_cache_id = static_cache.build_cache_id
+
+    @suppress
+    def dont_expose_as_web_action_base(self):
+        print("Called dont_expose_as_web_action on base, what happened?")
