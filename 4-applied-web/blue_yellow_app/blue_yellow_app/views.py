dc382f4497607252ee917ea186433b87823163cd
Michael Kennedy <mikeckennedy@gmail.com>
2016-10-10 17:11:03 -0700

    ready for writing the pyramid handlers code.

--- 4-applied-web/blue_yellow_app/blue_yellow_app/views.py
+++ 4-applied-web/blue_yellow_app/blue_yellow_app/views.py
@@ -1,6 +1,10 @@
 from pyramid.view import view_config
+import blue_yellow_app.infrastructure.static_cache as static_cache
 
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
-    return {'project': 'blue_yellow_app'}
+    return {
+        'project': 'blue_yellow_app',
+        'build_cache_id': static_cache.build_cache_id
+    }
