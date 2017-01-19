139ac7094fdf7e63d6e97bad5e04973dc548ad01
Michael Kennedy <mikeckennedy@gmail.com>
2017-01-19 14:12:43 -0800

    Starter code (copied from credit cards) for monitoring and logging.

---
+++ 17_logging_and_monitoring/blue_yellow_app_monitoring/blue_yellow_app/controllers/store_controller.py
@@ -0,0 +1,35 @@
+import pyramid_handlers
+
+from blue_yellow_app.controllers.base_controller import BaseController
+from blue_yellow_app.services.albums_service import AlbumsService
+from blue_yellow_app.services.store_service import StoreService
+
+
+class StoreController(BaseController):
+    @pyramid_handlers.action()
+    def complete(self):
+        # {'stripeToken': 'tok_19aUhV4bdW93NdzOnsPOc8K4',
+        # 'stripeTokenType': 'card',
+        # 'stripeEmail': 'mikeckennedy@gmail.com',
+        # 'action': 'complete',
+        # 'id': 28}
+
+        album_id = int(self.data_dict.get('id', -1))
+        album = AlbumsService.get_album_by_id(album_id)
+
+        token = self.data_dict.get('stripeToken')
+        StoreService.purchase_album(self.logged_in_user, album, album.price, token)
+
+        # TODO: send email receipt to self.data_dict['stripeEmail']
+
+        self.redirect('/store/success/{}'.format(album.id))
+
+    @pyramid_handlers.action(renderer='templates/store/success.pt')
+    def success(self):
+        album_id = int(self.data_dict.get('id', -1))
+        album = AlbumsService.get_album_by_id(album_id)
+        return {'album': album}
+
+    @pyramid_handlers.action(renderer='templates/store/failed.pt')
+    def failed(self):
+        return {}
---
+++ 17_logging_and_monitoring/blue_yellow_app_monitoring/blue_yellow_app/static/bower_components/bootstrap-css/README.md
@@ -0,0 +1,30 @@
+# Bootstrap
+
+
+This is a Bower component for [Bootstrap 3](http://getbootstrap.com/) CSS library (v3.3.6).
+
+## Installation:
+
+`bower install bootstrap-css`
+
+## Bootstrap 3.0 versions
+
+```
+bower install bootstrap-css#3.3.6
+bower install bootstrap-css#3.3.5
+bower install bootstrap-css#3.3.4
+bower install bootstrap-css#3.3.2
+bower install bootstrap-css#3.2.0
+bower install bootstrap-css#3.1.1
+bower install bootstrap-css#3.1.0
+bower install bootstrap-css#3.0.0
+```
+
+
+## Bootstrap 2.3 versions
+
+```
+bower install bootstrap-css#2.3.2
+bower install bootstrap-css#2.3.1
+bower install bootstrap-css#2.3.0
+```
