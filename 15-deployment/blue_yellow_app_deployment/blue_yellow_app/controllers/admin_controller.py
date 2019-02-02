6f2d1499d6e9dd3f5149240d9e568105b77af9c7
Michael Kennedy <mikeckennedy@gmail.com>
2019-02-02 08:42:46 -0800

    Disable some of the features or communicate more obviously in deployed app. #37

--- 15-deployment/blue_yellow_app_deployment/blue_yellow_app/controllers/admin_controller.py
+++ 15-deployment/blue_yellow_app_deployment/blue_yellow_app/controllers/admin_controller.py
@@ -18,18 +18,16 @@ class AdminController(BaseController):
                              request_method='POST',
                              name='new_album')
     def new_album_post(self):
-        vm = NewAlbumViewModel()
-        vm.from_dict(self.request.POST)
+        # Disable for deployed demo mode only
 
-        # might want to add this ;)
-        # if not vm.validate():
-        #     return vm.to_dict()
-
-        # Insert album
-        new_album = AlbumsService.create_album(vm.title, vm.year, vm.album_image,
-                                               vm.price, vm.url, vm.track_titles)
-        # log new album
-        print("Created a new album with id {}".format(new_album.id))
+        # vm = NewAlbumViewModel()
+        # vm.from_dict(self.request.POST)
+        #
+        # # Insert album
+        # new_album = AlbumsService.create_album(vm.title, vm.year, vm.album_image,
+        #                                        vm.price, vm.url, vm.track_titles)
+        # # log new album
+        # print("Created a new album with id {}".format(new_album.id))
 
         # redirect
-        self.redirect('/albums')
+        self.redirect('/admin/new_album')
--- 15-deployment/blue_yellow_app_deployment/blue_yellow_app/controllers/newsletter_controller.py
+++ 15-deployment/blue_yellow_app_deployment/blue_yellow_app/controllers/newsletter_controller.py
@@ -7,12 +7,17 @@ class NewsletterController(BaseController):
     # POST /newsletter/add_subscriber
     @pyramid_handlers.action(request_method='POST')
     def add_subscriber(self):
-        email = self.data_dict.get('email')
+        # Let's just make this seem to work on the live site
+        # Of course, we don't really run a website for Blue Rockets Band
+        # They are fictional :)
 
-        if MailingListService.add_subscriber(email):
-            self.redirect('/newsletter/subscribed')
+        # email = self.data_dict.get('email')
+        # if MailingListService.add_subscriber(email):
+        #     self.redirect('/newsletter/subscribed')
+        #
+        # self.redirect('/newsletter/failed')
 
-        self.redirect('/newsletter/failed')
+        self.redirect('/newsletter/subscribed')
 
     @pyramid_handlers.action(renderer='templates/newsletter/subscribed.pt')
     def subscribed(self):
